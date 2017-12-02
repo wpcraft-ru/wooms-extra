7a64c9337d09cb774a080bdbac65a8bb33dd01e3
Anatoly Yumashev <yumashev@fleep.io>
2017-12-02 22:45:37 +0300

    upd attributes sync

--- wooms-extra/inc/class-import-product-attributes.php
+++ wooms-extra/inc/class-import-product-attributes.php
@@ -1,27 +1,98 @@
 <?php
-
-
 /**
  * Update attributes for products
  */
 class WooMS_Product_Attributes
 {
-
   function __construct()
   {
-    add_action( 'admin_init', array($this, 'settings_init'), 150 );
-
     //Use hook do_action('wooms_product_update', $product_id, $value, $data);
-    add_action('wooms_product_update', [$this, 'load_data'], 10, 3);
+    add_action('wooms_product_update', [$this, 'update_data'], 10, 3);
 
-    add_filter('wooms_attributes', [$this, 'update_attributes'], 10, 3);
+    add_filter('wooms_attributes', [$this, 'update_base_attributes'], 10, 3);
+    add_filter('wooms_attributes', [$this, 'update_custtom_attributes'], 10, 3);
     add_filter('wooms_attributes', [$this, 'update_country'], 10, 3);
+
+    add_action( 'admin_init', array($this, 'settings_init'), 150 );
+  }
+
+  /**
+   * Sync attributes
+   */
+  public function update_data($product_id, $item, $data)
+  {
+      if(empty(get_option('wooms_attr_enabled'))){
+        return;
+      }
+
+      $product = wc_get_product($product_id);
+      $product_attributes = $product->get_attributes('edit');
+
+      if(empty($product_attributes)){
+        $product_attributes = array();
+      }
+
+      $product_attributes = apply_filters('wooms_attributes', $product_attributes, $product_id, $item, $data);
+
+      $product->set_attributes( $product_attributes );
+      $product->save();
+
+   }
+
+  //Update base attributes from MoySklad
+  function update_base_attributes($product_attributes, $product_id, $value){
+
+     if( ! empty($value['weight'])){
+       $attribute_object = new WC_Product_Attribute();
+  		 $attribute_object->set_name( "Вес" );
+  		 $attribute_object->set_options( array($value['weight']) );
+  		 $attribute_object->set_position( '0' );
+  		 $attribute_object->set_visible( 1 );
+  		 $attribute_object->set_variation( 0 );
+  		 $product_attributes[] = $attribute_object;
+     }
+
+     if( ! empty($value['volume'])){
+       $attribute_object = new WC_Product_Attribute();
+  		 $attribute_object->set_name( "Объем" );
+  		 $attribute_object->set_options( array($value['volume']) );
+  		 $attribute_object->set_position( '0' );
+  		 $attribute_object->set_visible( 1 );
+  		 $attribute_object->set_variation( 0 );
+  		 $product_attributes[] = $attribute_object;
+     }
+
+    return $product_attributes;
 
   }
 
-  function update_country($product_attributes, $product_id, $value){
+  // Custom fields update from MoySklad
+  function update_custtom_attributes($product_attributes, $product_id, $value)
+  {
 
+    if(empty($value['attributes'])){
+      return $product_attributes;
+    }
 
+    foreach ($value['attributes'] as $key => $value) {
+      $attribute_object = new WC_Product_Attribute();
+      $attribute_object->set_name( "Объем" );
+      $attribute_object->set_options( array($value['value']) );
+      $attribute_object->set_position( '0' );
+      $attribute_object->set_visible( 1 );
+      $attribute_object->set_variation( 0 );
+      $product_attributes[] = $attribute_object;
+    }
+
+    return $product_attributes;
+
+  }
+
+   /**
+   * Country - update
+   */
+  function update_country($product_attributes, $product_id, $value)
+  {
     if( empty($value['country']["meta"]["href"]) ) {
       return $product_attributes;
     } else {
@@ -33,113 +104,25 @@ class WooMS_Product_Attributes
     if(empty($data_api["name"])){
       return $product_attributes;
     } else {
-      $country = sanitize_meta($data_api["name"]);
+      $country = sanitize_text_field($data_api["name"]);
 
-      $product_attributes['country'] = [
-        'name' => 'Страна',
-        'value' => $country,
-        'position' => 0,
-        'is_visible' => 1,
-        'is_variation' => 0,
-        'is_taxonomy' => 0
-      ];
+      $attribute_object = new WC_Product_Attribute();
+      $attribute_object->set_name( "Страна" );
+      $attribute_object->set_options( array($country) );
+      $attribute_object->set_position( '0' );
+      $attribute_object->set_visible( 1 );
+      $attribute_object->set_variation( 0 );
+      $product_attributes[] = $attribute_object;
     }
 
     return $product_attributes;
-
   }
-
-
-  function update_attributes($product_attributes, $product_id, $value)
-  {
-
-    if(empty($value['attributes'])){
-      return $product_attributes;
-    }
-
-    // $product_attributes_current = get_post_meta($product_id, '_product_attributes', true);
-
-    foreach ($value['attributes'] as $key => $value) {
-      $product_attributes[$value['id']] = [
-        'name' => $value['name'],
-        'value' => $value['value'],
-        'position' => 0,
-        'is_visible' => 1,
-        'is_variation' => 0,
-        'is_taxonomy' => 0
-      ];
-    }
-
-    return $product_attributes;
-
-  }
-
-  /**
-   * Sync attributes
-   */
-  public function load_data($product_id, $item, $data)
-  {
-
-      if(empty(get_option('wooms_attr_enabled'))){
-        return false;
-      }
-
-      $atts = $this->get_atts($item);
-
-      if(empty($atts)){
-        return false;
-      }
-
-      $product_attributes = array();
-
-      foreach ($atts as $key => $value) {
-        $product_attributes[$key] = array(
-            'name' => $value['name'],
-            'value' => $value['value'],
-            'position' => 0,
-            'is_visible' => 1,
-            'is_variation' => 0,
-            'is_taxonomy' => 0
-        );
-
-      }
-
-      $product_attributes = apply_filters('wooms_attributes', $product_attributes, $product_id, $item, $data);
-
-      update_post_meta($product_id, '_product_attributes', $product_attributes);
-
-   }
-
-   //Prepare array attributes from data
-   function get_atts($data){
-
-     $atts = array();
-
-     if( ! empty($data['weight'])){
-       $atts['weight'] = [
-         'value' => $data['weight'],
-         'name' => "Вес",
-       ];
-     }
-
-     if( ! empty($data['volume'])){
-       $atts['volume'] = [
-         'value' => $data['volume'],
-         'name' => "Объем",
-       ];
-     }
-
-    return $atts;
-
-  }
-
 
   /**
    * Settings UI
    */
   public function settings_init()
   {
-
     register_setting('mss-settings', 'wooms_attr_enabled');
     add_settings_field(
       $id = 'wooms_attr_enabled',
@@ -148,7 +131,6 @@ class WooMS_Product_Attributes
       $page = 'mss-settings',
       $section = 'woomss_section_other'
     );
-
   }
 
   /**
--- wooms-extra/wooms-extra.php
+++ wooms-extra/wooms-extra.php
@@ -1,7 +1,7 @@
 <?php
 /*
 Plugin Name: WooMS Extra
-Version: 1.4.1
+Version: 1.4.2
 Plugin URI: https://wpcraft.ru/product/wooms-extra/
 Description: Расширение для синхронизации МойСклад и WooCommerce
 Author: WPCraft
