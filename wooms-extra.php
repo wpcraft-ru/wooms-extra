74ff5a39a77eb3ff8fdc7a08033ffeadf8ccbdd9
Artem Abramovich <artikus.sol@gmail.com>
2018-09-06 20:58:54 +0400

    Update version and readme

--- wooms-extra.php
+++ wooms-extra.php
@@ -7,7 +7,7 @@
  * Author URI: https://wpcraft.ru/
  * Developer: WPCraft
  * Developer URI: https://wpcraft.ru/
- * Version: 1.8.1
+ * Version: 1.8.2
  * Text Domain: wooms-xt
  * Domain Path: /languages
  *
