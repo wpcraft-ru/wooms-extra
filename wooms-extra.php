6b26cba73f5afb3b3305274fb00efa36d72ffd5d
Artem Abramovich <artikus.sol@gmail.com>
2018-10-29 14:35:15 +0400

    Update version and readme

--- wooms-extra.php
+++ wooms-extra.php
@@ -12,7 +12,7 @@
  * Domain Path: /languages
  *
  * WC requires at least: 3.0
- * WC tested up to: 3.4.7
+ * WC tested up to: 3.5.0
  *
  * WooMS requires at least: 2.0.5
  * WooMS tested up to: 2.0.5
