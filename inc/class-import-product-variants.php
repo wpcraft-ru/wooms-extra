7e7400e2734a96b11ed7d1262ed9d222de94dabf
Artem Abramovich <artikus.sol@gmail.com>
2018-12-04 02:47:46 +0400

    Rename title & button from management. #104

--- inc/class-import-product-variants.php
+++ inc/class-import-product-variants.php
@@ -726,18 +726,18 @@ class WooMS_Product_Variations {
 			return;
 		}
 
-		echo '<h2>Синхронизация вариативных товаров</h2>';
+		echo '<h2>Модификации (вариации)</h2>';
 		if ( empty( get_transient( 'wooms_variant_start_timestamp' ) ) ) {
 			echo "<p>Нажмите на кнопку ниже, чтобы запустить синхронизацию данных о вариативных товарах вручную</p>";
 			echo "<p><strong>Внимание!</strong> Синхронизацию вариативных товаров необходимо поводить <strong>после</strong> общей синхронизации товаров</p>";
 			if (empty( get_transient( 'wooms_start_timestamp' ) )){
-				printf( '<a href="%s" class="button button-primary">Старт импорта вариаций</a>', add_query_arg( 'a', 'wooms_import_variations_manual_start', admin_url( 'admin.php?page=moysklad' ) ) );
+				printf( '<a href="%s" class="button button-primary">Выполнить</a>', add_query_arg( 'a', 'wooms_import_variations_manual_start', admin_url( 'admin.php?page=moysklad' ) ) );
 			} else {
-				printf( '<span href="%s" class="button button-secondary" style="display:inline-block">Старт импорта вариаций</span>', add_query_arg( 'a', 'wooms_import_variations_manual_start', admin_url( 'admin.php?page=moysklad' ) ) );
+				printf( '<span href="%s" class="button button-secondary" style="display:inline-block">Выполнить</span>', add_query_arg( 'a', 'wooms_import_variations_manual_start', admin_url( 'admin.php?page=moysklad' ) ) );
 			}
 
 		} else {
-			printf( '<a href="%s" class="button button-secondary">Остановить импорт вариаций</a>', add_query_arg( 'a', 'wooms_import_variations_manual_stop', admin_url( 'admin.php?page=moysklad' ) ) );
+			printf( '<a href="%s" class="button button-secondary">Остановить</a>', add_query_arg( 'a', 'wooms_import_variations_manual_stop', admin_url( 'admin.php?page=moysklad' ) ) );
 		}
 	}
 
