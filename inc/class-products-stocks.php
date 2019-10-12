9ac15dc0423d97106d46a19cb41798683c1ca0a7
R2 D2 <uptimizt@gmail.com>
2019-10-13 01:45:42 +0300

    small fix for stock data

--- inc/class-products-stocks.php
+++ inc/class-products-stocks.php
@@ -217,10 +217,9 @@ class Stocks {
                 $stock = (int)$data['rows'][0][$stock_type];
             }
 
-
             $stock_log = array(
-                'quantity' => $data['rows'][0]['quantity'],
-                'stock'    => $data['rows'][0]['stock'],
+                'quantity' => empty($data['rows'][0]['quantity']) ? 0 : $data['rows'][0]['quantity'],
+                'stock'    => empty($data['rows'][0]['stock']) ? 0 : $data['rows'][0]['stock'],
             );
         }
 
